a52266d5cc400b3d82cb7d2b8cc9fb9c41e1237d
Daniel Andrew <dwandrew@hotmail.com>
2020-02-26 13:40:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ end
 def reduce (array, starting_value = '')
   index = 0
   while index<array.length
-    total = yield(array[index], starting_value)
+    total = yield(array[index], starting_value[index])
     index+=1
   end
   total
