b8ffa3132b7074cd4b03ae37de53a839d7fca69a
Daniel Andrew <dwandrew@hotmail.com>
2020-02-26 10:45:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,8 +13,8 @@ def reduce (array, other = nil)
   total = other
   index = 0
   while index< input.length
-  new.push(yield(input[index]))
+  yield(array[index]))
   index+=1
   end
-  return new
+  
 end
