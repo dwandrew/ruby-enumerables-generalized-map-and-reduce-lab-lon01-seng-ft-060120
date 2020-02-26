1dff7c24566f3a34489ea54b0ec3a73bfe20c825
Daniel Andrew <dwandrew@hotmail.com>
2020-02-26 13:57:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,10 +12,10 @@ end
 def reduce (array, starting_value = nil)
   index = 0
   while index<array.length
-  if starting_value == nil
-    total = yield(array[index])
-  else
+  if starting_value != nil
     total = yield(array[index], starting_value)
+  else
+    total = yield(array[index])
   end
     index+=1
   end
